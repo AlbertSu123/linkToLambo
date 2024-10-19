48dcae45721bfbb6541fc8aea2bf4b9fbca896c5
Albert Su <albertsu2001@gmail.com>
2024-10-18 22:53:58 -0700

    ✅Working

--- pages/redeem/[slug].tsx
+++ pages/redeem/[slug].tsx
@@ -1,13 +1,60 @@
 import Page from '@/components/page'
 import Section from '@/components/section'
 import RedeemButton from '@/components/transactions/RedeemButton'
-import { DynamicWidget, useIsLoggedIn } from '@dynamic-labs/sdk-react-core'
+import { usdcContractAddress, usdcContractAbi } from '@/lib/contracts/USDC'
+import { isEthereumWallet } from '@dynamic-labs/ethereum'
+import {
+	DynamicWidget,
+	useDynamicContext,
+	useIsLoggedIn,
+} from '@dynamic-labs/sdk-react-core'
+import { Address, encodeAbiParameters, keccak256 } from 'viem'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { linkToLamboAddress, linkToLamboAbi } from '@/lib/contracts/LinkToLambo'
+import TokenDisplay, { Token } from '@/components/token-display'
 
 export default function Redeem() {
 	const router = useRouter()
 	const password = router.query.slug as string
 	const isLoggedIn = useIsLoggedIn()
+	const { primaryWallet } = useDynamicContext()
+	const [tokenBalances, setTokenBalances] = useState<Token>()
+
+	useEffect(() => {
+		const fetchTokenBalance = async () => {
+			if (primaryWallet && isEthereumWallet(primaryWallet)) {
+				const client = await primaryWallet.getPublicClient()
+				const amount = await client.readContract({
+					address: linkToLamboAddress,
+					abi: linkToLamboAbi,
+					functionName: 'tokenAmounts',
+					args: [
+						keccak256(encodeAbiParameters([{ type: 'string' }], [password])),
+					],
+				})
+				const tokenAddress = (await client.readContract({
+					address: linkToLamboAddress,
+					abi: linkToLamboAbi,
+					functionName: 'tokenAddresses',
+					args: [
+						keccak256(encodeAbiParameters([{ type: 'string' }], [password])),
+					],
+				})) as Address
+				const name = await client.readContract({
+					address: tokenAddress,
+					abi: usdcContractAbi,
+					functionName: 'name',
+				})
+				setTokenBalances({
+					address: tokenAddress,
+					amount: Number(amount),
+					name: name,
+				})
+			}
+		}
+		fetchTokenBalance()
+	}, [primaryWallet])
 
 	return (
 		<Page>
@@ -23,6 +70,13 @@ export default function Redeem() {
 					<div className='flex justify-center mt-6'>
 						<DynamicWidget variant='modal' />
 					</div>
+				)}
+				{tokenBalances && (
+					<TokenDisplay
+						address={tokenBalances.address}
+						amount={tokenBalances.amount}
+						name={tokenBalances.name}
+					/>
 				)}
 			</Section>
 		</Page>
